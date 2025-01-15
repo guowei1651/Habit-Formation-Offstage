4a4519dbe46b87676c32516d9e5bffb8eb194128
wales guo <guowei1651@gmail.com>
2025-01-15 21:31:18 +0800

    v2

--- database/init.go
+++ database/init.go
@@ -7,7 +7,7 @@ import (
 	"database/sql"
 )
 
-type DB interface {
+type DBProcess interface {
 	ConnectPing()
 	ConnectOpen()
 	GetPool() (db *sql.DB)
@@ -18,7 +18,7 @@ var DBConnectPool *sql.DB
 func OpenConnectPool () {
 	log.Printf("开始创建数据库连接")
 	log.Printf("数据库配置参数:%v", hfConfig.Config)
-	var pool *DB
+	var pool *DBProcess
 	if (hfConfig.Config.DBConfig.Schmea == "postgres") {
 		pool = &Postgres{}
 	} else if (hfConfig.Config.DBConfig.Schmea == "mysql") {
