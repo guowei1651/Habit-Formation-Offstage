809d35c1edb48ebcb6cdf2bfbc2e4c569af08eab
wales guo <guowei1651@gmail.com>
2025-01-15 20:11:12 +0800

    v2

--- database/init.go
+++ database/init.go
@@ -2,13 +2,13 @@ package database
 
 import (
 	"log"
-	"hf/config"
+	hfConfig "hf/config"
 )
 
 func OpenConnectPool () {
 	log.Printf("开始创建数据库连接")
 
-	log.Printf("数据库配置参数:%v", config.config)
+	log.Printf("数据库配置参数:%v", hfConfig.config)
 	sqlOpen()
 
 	log.Printf("创建数据库连接结束")
