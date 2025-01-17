1962a8c7e3fb1b191d885bece018e8f141228f66
wales guo <guowei1651@gmail.com>
2025-01-17 18:00:48 +0800

    v2

--- web/service/userService.go
+++ web/service/userService.go
@@ -5,7 +5,7 @@ import (
 	dao "hf/web/dao"
 )
 
-func Login(username string, password string) (dao.User, error){
+func Login(username string, password string) (*dao.User, error){
 	log.Printf("用户登录， username:%v password:%v", username, password)
 	return dao.Login(username, password)
 }
