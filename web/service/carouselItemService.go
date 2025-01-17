bbb43bdef45beb0364afd0b35fef39e7af7c71db
wales guo <guowei1651@gmail.com>
2025-01-17 17:37:29 +0800

    v2

--- web/service/carouselItemService.go
+++ web/service/carouselItemService.go
@@ -5,7 +5,7 @@ import (
 	dao "hf/web/dao"
 )
 
-func findAllCarouselItemsByCarouseId(carouseId int) ([]CarouselItem, error) {
+func FindAllCarouselItemsByCarouseId(carouseId int) ([]CarouselItem, error) {
 	log.Printf("查询轮播项 carouseId:%v", carouseId)
 	return dao.FindAllCarouselItemsByCarouselId(carouseId)
 }
