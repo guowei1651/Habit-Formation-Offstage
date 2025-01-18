44e496750e7d222797fab331539207fcc608e995
wales guo <guowei1651@gmail.com>
2025-01-18 12:57:25 +0800

    v2

--- web/utils/auth.go
+++ web/utils/auth.go
@@ -6,8 +6,5 @@ import (
 )
 
 func GetUserId(request *restful.Request) string {
-	for k, v := range request.Request.Header {
-        fmt.Printf("Header field %q, Value %q\n", k, v)
-    }
-	return request.Request.Header.Get("HF-User-Id")
+	return request.HeaderParameter("HF-User-Id")
 }
