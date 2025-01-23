8f5ff4e2aa94627f4189cb4060bdafa9e8f2d070
wales guo <guowei1651@gmail.com>
2025-01-23 21:06:40 +0800

    v2

--- web/dao/carouselItemDao.go
+++ web/dao/carouselItemDao.go
@@ -3,16 +3,17 @@ package dao
 import (
     "log"
     "strconv"
+    "database/sql"
     db "hf/database"
 )
 
 type CarouselItem struct {
-	Order		int    	`json:"order" description:"Carousel Item on carousel order" default:"1"`
-	Genus		string 	`json:"type" description:"type of the CarouselItem" default:"image"`
-    RelationsId string  `json:"relations_id" description:"relations_id of the CarouselItem" default:"0"`
-	AlertLevel  string	`json:"alert_level" description:"alert_level of the CarouselItem" default:"norme"`
-	TriggerTime string	`json:"trigger_time" description:"trigger_time of the CarouselItem" default:""`
-	Duration	int 	`json:"duration" description:"duration of the CarouselItem" default:"30"`
+	Order		int		`json:"order" description:"Carousel Item on carousel order" default:"1"`
+	Genus		string	`json:"type" description:"type of the CarouselItem" default:"image"`
+	RelationsId	int		`json:"relations_id" description:"relations_id of the CarouselItem" default:"0"`
+	AlertLevel	string	`json:"alert_level" description:"alert_level of the CarouselItem" default:"norme"`
+	TriggerTime	string	`json:"trigger_time" description:"trigger_time of the CarouselItem" default:""`
+	Duration	int		`json:"duration" description:"duration of the CarouselItem" default:"30"`
 	ChartUrl	string 	`json:"chartUrl" description:"chartUrl of the CarouselItem" default:""`
 }
 
@@ -33,25 +34,25 @@ WHERE carousel_id = $1 AND delete_flag = FALSE ORDER BY carousel_item.order;`, c
 
     for rows.Next() {
         var ci CarouselItem
-        var order string
-        var genus string
-        var relationsId string
-        var alertLevel string
-        var triggerTime string
-        var duration string
-        var chartUrl string
+        var order sql.NullInt32
+        var genus sql.NullString
+        var relationsId sql.NullInt32
+        var alertLevel sql.NullString
+        var triggerTime sql.NullString
+        var duration sql.NullInt32
+        var chartUrl sql.NullString
         if err := rows.Scan(&order, &genus, &relationsId, &alertLevel, &triggerTime, &duration, &chartUrl); err != nil {
             log.Fatal("db row next error. err->", err)
             return carouselItems, err
         }
         log.Printf("row ->", order, genus, duration, chartUrl)
-        ci.Order, _ = strconv.Atoi(order)
-        ci.Genus = genus
-        ci.RelationsId = relationsId
-        ci.AlertLevel = alertLevel
-        ci.TriggerTime = triggerTime
-        ci.Duration, _ = strconv.Atoi(duration)
-        ci.ChartUrl = chartUrl
+        ci.Order = (order.Valid ? order.value : 0)
+        ci.Genus = (genus.Valid ? genus.value : "0")
+        ci.RelationsId = (relationsId.Valid ? relationsId.value : 0)
+        ci.AlertLevel = (alertLevel.Valid ? alertLevel.value : "0")
+        ci.TriggerTime = (triggerTime.Valid ? triggerTime.value : "")
+        ci.Duration = (duration.Valid ? duration.value : 0)
+        ci.ChartUrl = (chartUrl.Valid ? chartUrl.value : "")
         carouselItems = append(carouselItems, ci)
     }
     if err = rows.Err(); err != nil {
