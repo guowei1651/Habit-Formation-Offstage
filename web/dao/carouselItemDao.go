f2c3743128ad833699efae796ddd71b195b2e3dd
wales guo <guowei1651@gmail.com>
2025-01-23 21:25:41 +0800

    v2

--- web/dao/carouselItemDao.go
+++ web/dao/carouselItemDao.go
@@ -45,13 +45,13 @@ WHERE carousel_id = $1 AND delete_flag = FALSE ORDER BY carousel_item.order;`, c
             return carouselItems, err
         }
         log.Printf("row ->", order, genus, duration, chartUrl)
-        if (order.Valid) { ci.Order = order.Value() } else{ ci.Order = 0 }
-        if (genus.Valid) { ci.Genus = genus.Value() } else{ ci.Genus = "0" }
-        if (relationsId.Valid) { ci.RelationsId = relationsId.Value() } else{ ci.RelationsId = 0 }
-        if (alertLevel.Valid) { ci.AlertLevel = alertLevel.Value() } else{ ci.AlertLevel = "0" }
-        if (triggerTime.Valid) { ci.TriggerTime = triggerTime.Value() } else{ ci.TriggerTime = "" }
-        if (duration.Valid) { ci.Duration = duration.Value() } else{ ci.Duration = 0 }
-        if (chartUrl.Valid) { ci.ChartUrl = chartUrl.Value() } else{ ci.ChartUrl = "" }
+        if (order.Valid) { ci.Order, _ = order.Value() } else{ ci.Order = 0 }
+        if (genus.Valid) { ci.Genus, _ = genus.Value() } else{ ci.Genus = "0" }
+        if (relationsId.Valid) { ci.RelationsId, _ = relationsId.Value() } else{ ci.RelationsId = 0 }
+        if (alertLevel.Valid) { ci.AlertLevel, _ = alertLevel.Value() } else{ ci.AlertLevel = "0" }
+        if (triggerTime.Valid) { ci.TriggerTime, _ = triggerTime.Value() } else{ ci.TriggerTime = "" }
+        if (duration.Valid) { ci.Duration, _ = duration.Value() } else{ ci.Duration = 0 }
+        if (chartUrl.Valid) { ci.ChartUrl, _ = chartUrl.Value() } else{ ci.ChartUrl = "" }
         carouselItems = append(carouselItems, ci)
     }
     if err = rows.Err(); err != nil {
